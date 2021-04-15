c0a3606b0a6153e0b19611ca3410bc7299fd1a88
jakobap <jakobpoerschmann@gmail.com>
2021-04-16 00:24:15 +0200

    validation framework

--- model_code/item_based_1.py
+++ model_code/item_based_1.py
@@ -1,36 +1,62 @@
 import pandas as pd
 from src.model_framework import RecommenderFramework
+import json
 
 
 class ItemBased(RecommenderFramework):
-    def __init__(self, user_df, item_df, user_item_df, user_id, item_id):
-        super().__init__(user_df, item_df, user_item_df, user_id, item_id)
+    def __init__(self, user_df, item_df, clean_data, user_item_df, user_id, item_id):
+        super().__init__(user_df, item_df, clean_data, user_item_df, user_id, item_id)
 
-    def make_recommendation(self, user_id):
-        user_index = self.user_dict[user_id]
-        print('computing similarities...')
+    def make_recommendation(self, user_id=None, user_profile=None):
+        if user_id is not None and user_profile is not None:
+            raise Exception('Recommendation must be based on either ID or Profile, but never both!')
+        elif user_id is not None:
+            user_index = self.user_dict[user_id]
+            print('computing similarities...')
+            # similarities_cos = [self._cosine_dist(self.user_df.iloc[user_index, :], i[1]) for i in
+            #                     self.item_df.iterrows()]
+            # similarities_euc = [self._euclidean_dist(self.user_df.iloc[user_index, :], i[1]) for i in
+            #                     self.item_df.iterrows()]
+            similarities_count = [self._count_dist(self.user_df.iloc[user_index, :], i[1]) for i in
+                                  self.item_df.iterrows()]
+        elif user_profile is not None:
+            print('computing similarities...')
+            # similarities_cos = [self._cosine_dist(user_profile, i[1]) for i in
+            #                     self.item_df.iterrows()]
+            # similarities_euc = [self._euclidean_dist(user_profile, i[1]) for i in
+            #                     self.item_df.iterrows()]
+            similarities_count = [self._count_dist(user_profile, i[1]) for i in
+                                  self.item_df.iterrows()]
 
-        similarities_cos = [self._cosine_dist(self.user_df.iloc[user_index, :], i[1]) for i in self.item_df.iterrows()]
-        similarities_euc = [self._eucledian_dist(self.user_df.iloc[user_index, :], i[1]) for i in self.item_df.iterrows()]
-        similarities_count = [self._count_dist(self.user_df.iloc[user_index, :], i[1]) for i in self.item_df.iterrows()]
-        import matplotlib.pyplot as plt
-        plt.title('Cosin Sim')
-        plt.hist(similarities_cos)
-        plt.show()
-        plt.title('Eucl Sim')
-        plt.hist(similarities_euc)
-        plt.show()
-        plt.title('Count Sim')
-        plt.hist(similarities_count)
-        plt.show()
+        # import matplotlib.pyplot as plt
+        # plt.title('Cosin Sim')
+        # plt.hist(similarities_cos)
+        # plt.show()
+        # plt.title('Eucl Sim')
+        # plt.hist(similarities_euc)
+        # plt.show()
+        # plt.title('Count Sim')
+        # plt.hist(similarities_count)
+        # plt.show()
+
+        return []
 
 
 if __name__ == '__main__':
     user_m = pd.read_csv('../data/user_m.csv')
     item_m = pd.read_csv('../data/item_m.csv')
-    rec = ItemBased(user_df=user_m, item_df=item_m, user_item_df=None, user_id='CUSTOMER_ID', item_id='PRODUCT_ID')
+    cleandata_m = pd.read_csv('../data/cleandata.csv')
 
-    # rec.eval()
+    with open('../data/invoice_by_customer_dict.json') as json_file:
+        invoice_by_customer_dict = json.load(json_file)
+
+    with open('../data/product_by_invoice_dict.json') as json_file:
+        product_by_invoice_dict = json.load(json_file)
+
+    rec = ItemBased(user_df=user_m, item_df=item_m, clean_data=cleandata_m, user_item_df=None,
+                    user_id='CUSTOMER_ID', item_id='PRODUCT_ID')
+
+    rec.eval(invoice_by_customer_dict, product_by_invoice_dict)
 
     rec.make_recommendation('+6F9xZ4in/zBbuY9ZysWwnoH0yxqbg7GFSK/9wNk+24=')
 
--- src/model_framework.py
+++ src/model_framework.py
@@ -1,13 +1,13 @@
-import pandas as pd
 from scipy.spatial import distance
-import random
 import numpy as np
+import random
 
 
 class RecommenderFramework:
-    def __init__(self, user_df, item_df, user_item_df, user_id, item_id):
+    def __init__(self, user_df, item_df, clean_data, user_item_df, user_id, item_id):
         self.user_df, self.item_df, self.user_item_df = user_df, item_df, user_item_df  # user, item & user-item matrix
-        self.user_id, self.item_id = user_id, item_id # columns that contains user & item identifiers
+        self.clean_df = clean_data
+        self.user_id, self.item_id = user_id, item_id  # columns that contains user & item identifiers
 
         self._identifiers()
 
@@ -26,8 +26,8 @@ class RecommenderFramework:
         return distance.cosine(u, v)
 
     @staticmethod
-    def _eucledian_dist(u, v):
-        """Eucledian Distance between two arrays"""
+    def _euclidean_dist(u, v):
+        """Euclidean Distance between two arrays"""
         return distance.euclidean(u, v)
 
     @staticmethod
@@ -39,39 +39,38 @@ class RecommenderFramework:
         """Separate User-Item-Matrix in train test split"""
         return
 
-    def eval(self):
+    def eval(self, invoice_by_customer_dict, product_by_invoice_dict):
         """Evaluate Recommendations based on validation set"""
-        deviation_list_sq = []
-        deviation_list_ab = []
         n = 1000  # number of evaluation iterations
 
-        # for i in range(0, n):
-        #     random_u = random.choice(self.user_df)  # select a random user from user pool
-        #     user_ratings_real = matrix.loc[random_u]  # extract documented ratings for chosen user
-        #     items_rated = user_ratings_real[user_ratings_real > 0].index  # extract items that have been rated by user
-        #     test_item = random.choice(items_rated)  # choose random item to evaluate from rated items
-        #
-        #     original_rating = matrix.loc[random_u, test_item]  # define originally documented rating
-        #     matrix_mock = matrix.copy()  # define copy as evaluation matrix
-        #     matrix_mock.loc[random_u, test_item] = 0  # manipulate mock matrix as if item has never been rated by user
-        #
-        #     #### Algo 1 ####
-        #     # predicted_score = recommender(matrix_mock, random_u)[1][test_item]
-        #     #### Algo 2 ####
-        #     # predicted_score = getRecommendations(matrix_mock.transpose(), random_u, corr_matrix)[1][test_item]
-        #     #### Algo 3 ####
-        #     predicted_score = getRecommendations2(matrix_mock, random_u, corr_matrix)[1][test_item]
-        #
-        #     deviation_list_sq.append(
-        #         (original_rating - predicted_score) ** 2)  # append squared deviation to deviation list (squared)
-        #     deviation_list_ab.append(
-        #         (original_rating - predicted_score))  # append squared deviation to deviation list (not squared)
-        #
-        # mse = sum(deviation_list_sq) / n
-        # mae = sum(deviation_list_ab) / n
-        #
-        # print(f'Mean Squared Error = {mse}')
-        # print(f'Mean Absolute Error = {mae}')
+        for i in range(n):
+            # choose random user id to test on
+            check_user_id = random.choice(list(invoice_by_customer_dict.keys()))
+
+            # random chose invoice that is attributed to user
+            check_invoice_id = random.choice(invoice_by_customer_dict[check_user_id])
+
+            # random chose product that the user purchased
+            check_product_id = random.choice(product_by_invoice_dict[str(check_invoice_id)])
+
+            # get all other product ids that the user purchased
+            # for invoice in invoice_by_customer_dict[check_user_id]:
+
+            prod_id_list = [product_by_invoice_dict[str(i)] for i in invoice_by_customer_dict[check_user_id]]
+            prod_id_list = list(np.concatenate(prod_id_list).flat)
+            prod_id_list.remove(check_product_id)
+
+            # get attributes from all other products purchased by the user
+            item_indices = set([self.item_dict[item] for item in prod_id_list])
+            item_attributes = self.item_df.loc[item_indices, :]
+
+            # aggregate attributes to user profile
+            user_profile = item_attributes.max().drop(columns=['PRODUCT_ID'])
+
+            # compare to prediction
+            self.make_recommendation(user_profile=user_profile)
+            # TODO: Make sure that recommendations are attributable to product IDs
+
         return
 
     def predict(self):
